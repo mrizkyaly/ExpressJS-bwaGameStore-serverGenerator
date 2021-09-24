c81998d90e80949af84975db15d6578ceae1c4f7
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-09-24 16:57:02 +0700

    create read,update,delete category

--- app/category/controller.js
+++ app/category/controller.js
@@ -2,7 +2,9 @@ const Category = require('./model');
 module.exports = {
     index: async (req, res) => {
         try {
-            res.render('admin/category/view_category');
+            const category = await Category.find();
+            res.render('admin/category/view_category', { category });
+            console.log(category);
         } catch (error) {
             console.log(err);
         }
@@ -20,6 +22,41 @@ module.exports = {
             let category = await Category({ name });
             await category.save();
 
+            res.redirect('/category');
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const category = await Category.findOne({ _id: id });
+            res.render('admin/category/edit', { category });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { name } = req.body;
+            const category = await Category.findOneAndUpdate(
+                {
+                    _id: id,
+                },
+                { name }
+            );
+            res.redirect('/category');
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    actionDelete: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const category = await Category.findOneAndRemove({
+                _id: id,
+            });
             res.redirect('/category');
         } catch (error) {
             console.log(error);
