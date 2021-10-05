c16ae2f3e3aa2ed19bc221b9907d3b9137583e8c
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-10-05 15:02:30 +0700

    Create API Signup

---
+++ app/auth/controller.js
@@ -0,0 +1,66 @@
+const Player = require('../player/model');
+const path = require('path');
+const fs = require('fs');
+const config = require('../../config');
+
+module.exports = {
+    signup: async (req, res, next) => {
+        try {
+            const payload = req.body;
+
+            if (req.file) {
+                let tmp_path = req.file.path;
+                let originExt =
+                    req.file.originalname.split('.')[
+                        req.file.originalname.split('.').length - 1
+                    ];
+                let filename = req.file.filename + '.' + originExt;
+                let target_path = path.resolve(
+                    config.rootPath,
+                    `public/uploads/${filename}`
+                );
+
+                const src = fs.createReadStream(tmp_path);
+                const dest = fs.createWriteStream(target_path);
+
+                src.pipe(dest);
+                src.on('end', async () => {
+                    try {
+                        const player = new Player({
+                            ...payload,
+                            avatar: filename,
+                        });
+
+                        await player.save();
+
+                        delete player._doc.password;
+                        res.status(201).json({ data: player });
+                    } catch (error) {
+                        if (error && error.name === 'ValidationError') {
+                            return res.status(422).json({
+                                error: 1,
+                                message: error.message,
+                                fields: error.errors,
+                            });
+                        }
+                        next(error);
+                    }
+                });
+            } else {
+                let player = new Player(payload);
+                await player.save();
+                delete player._doc.password;
+                res.status(201).json({ data: player });
+            }
+        } catch (error) {
+            if (error && error.name === 'ValidationError') {
+                return res.status(422).json({
+                    error: 1,
+                    message: error.message,
+                    fields: error.errors,
+                });
+            }
+            next(error);
+        }
+    },
+};
