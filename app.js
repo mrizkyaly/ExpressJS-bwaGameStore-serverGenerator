80fcfcc5770b43a2cfffb26dc17a8e56a5898302
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-10-05 11:34:09 +0700

    create landing page & detail page API

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ const bankRouter = require('./app/bank/router');
 const paymentRouter = require('./app/payment/router');
 const usersRouter = require('./app/users/router');
 const transactionRouter = require('./app/transaction/router');
+const playerRouter = require('./app/player/router');
 
 var app = express();
-
+const URL = '/api/v1';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -51,6 +52,8 @@ app.use('/bank', bankRouter);
 app.use('/payment', paymentRouter);
 app.use('/transaction', transactionRouter);
 
+// API
+app.use(`${URL}/players`, playerRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
