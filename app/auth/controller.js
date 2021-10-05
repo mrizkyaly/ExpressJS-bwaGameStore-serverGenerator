25d2745eab814c04b505d64ad963b79ff708fea5
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-10-05 21:52:07 +0700

    create API SIgnIn & Category

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require('../player/model');
 const path = require('path');
 const fs = require('fs');
 const config = require('../../config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     signup: async (req, res, next) => {
@@ -10,11 +12,11 @@ module.exports = {
 
             if (req.file) {
                 let tmp_path = req.file.path;
-                let originExt =
+                let originaExt =
                     req.file.originalname.split('.')[
                         req.file.originalname.split('.').length - 1
                     ];
-                let filename = req.file.filename + '.' + originExt;
+                let filename = req.file.filename + '.' + originaExt;
                 let target_path = path.resolve(
                     config.rootPath,
                     `public/uploads/${filename}`
@@ -24,6 +26,7 @@ module.exports = {
                 const dest = fs.createWriteStream(target_path);
 
                 src.pipe(dest);
+
                 src.on('end', async () => {
                     try {
                         const player = new Player({
@@ -34,33 +37,85 @@ module.exports = {
                         await player.save();
 
                         delete player._doc.password;
+
                         res.status(201).json({ data: player });
-                    } catch (error) {
-                        if (error && error.name === 'ValidationError') {
+                    } catch (err) {
+                        if (err && err.name === 'ValidationError') {
                             return res.status(422).json({
                                 error: 1,
-                                message: error.message,
-                                fields: error.errors,
+                                message: err.message,
+                                fields: err.errors,
                             });
                         }
-                        next(error);
+                        next(err);
                     }
                 });
             } else {
                 let player = new Player(payload);
+
                 await player.save();
+
                 delete player._doc.password;
+
                 res.status(201).json({ data: player });
             }
-        } catch (error) {
-            if (error && error.name === 'ValidationError') {
+        } catch (err) {
+            if (err && err.name === 'ValidationError') {
                 return res.status(422).json({
                     error: 1,
-                    message: error.message,
-                    fields: error.errors,
+                    message: err.message,
+                    fields: err.errors,
                 });
             }
-            next(error);
+            next(err);
         }
+    },
+
+    signin: (req, res, next) => {
+        const { email, password } = req.body;
+
+        Player.findOne({ email: email })
+            .then((player) => {
+                if (player) {
+                    const checkPassword = bcrypt.compareSync(
+                        password,
+                        player.password
+                    );
+                    if (checkPassword) {
+                        const token = jwt.sign(
+                            {
+                                player: {
+                                    id: player.id,
+                                    username: player.username,
+                                    email: player.email,
+                                    nama: player.nama,
+                                    phoneNumber: player.phoneNumber,
+                                    avatar: player.avatar,
+                                },
+                            },
+                            config.jwtKey
+                        );
+
+                        res.status(200).json({
+                            data: { token },
+                        });
+                    } else {
+                        res.status(403).json({
+                            message: 'password yang anda masukan salah.',
+                        });
+                    }
+                } else {
+                    res.status(403).json({
+                        message: 'email yang anda masukan belum terdaftar.',
+                    });
+                }
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    message: err.message || `Internal server error`,
+                });
+
+                next();
+            });
     },
 };
