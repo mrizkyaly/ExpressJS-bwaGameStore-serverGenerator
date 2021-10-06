13cff13aef546a0bace27061f7a128696b0d3e4d
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-10-06 10:17:05 +0700

    create API History Detail

--- app/player/controller.js
+++ app/player/controller.js
@@ -176,4 +176,19 @@ module.exports = {
             });
         }
     },
+    historyDetail: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const history = await Transaction.findOne({ _id: id });
+            if (!history)
+                return res
+                    .status(404)
+                    .json({ message: 'history tidak ditemukan' });
+            res.status(200).json({ data: history });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message || `Internal server error`,
+            });
+        }
+    },
 };
